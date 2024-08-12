9a9f5841621cf0a48c965ac8698f501f169e575c
akash.majida <akash.majida@gmail.com>
2024-08-12 15:51:03 +0000

    forms and lazyloading

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  constructor(){}
 
+  register(regForm:NgForm){
+    console.log(regForm.value)
+    
+
+  }
+  reset(regForm:NgForm){
+    regForm.reset()
+  }
 }
